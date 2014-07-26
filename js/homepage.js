20a1567f05248fadb1c4df0261a177164759deed
kusmour <kxxxxx>
2014-07-27 01:33:33 +0800

    add animation

--- js/homepage.js
+++ js/homepage.js
@@ -8,4 +8,13 @@ $(document).ready( function() {
 		width_ = $(document.body).width();
 		$("#banner_photo").css("width", width_ + "px");
 	});
+
+	$(".author_social").mouseover(function() {
+		$(this).css("font-size", 22 + "px");
+		//alert("hello~");
+	});
+	$(".author_social").mouseleave(function() {
+		$(this).css("font-size", 18 + "px");
+	});
+
 });

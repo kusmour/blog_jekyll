b7b61bae1523bfd32d2ab63bf1a20ff7215c22d2
kusmour <kxxxxx>
2014-08-13 23:12:54 +0800

    article page js

--- js/article_page.js
+++ js/article_page.js
@@ -1,11 +1,11 @@
 $(document).ready( function() {
 	//alert("hello");
 	var width_ = $(document.body).width() * 0.7;
-	$("#illustration").css("width", width_ + "px");
+	$(".illustration").css("width", width_ + "px");
 
 	$( window ).resize( function() {
 		//alert($(document).width());
 		width_ = $(document.body).width();
-		$("#illustration").css("width", width_ + "px");
+		$(".illustration").css("width", width_ + "px");
 	});
 });

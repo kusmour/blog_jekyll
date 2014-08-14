36f236d22e0624dd71846255d0e7a6d18b29ab68
kusmour <kxxxxx>
2014-08-14 19:38:43 +0800

    finish horizon page

--- js/parallax.js
+++ js/parallax.js
@@ -1,16 +1,60 @@
 $(document).ready(function() {
 	
+	redrawDotNav();
+	
 	/* Scroll event handler */
     $(window).bind('scroll',function(e){
-		parallaxScroll();
+    	parallaxScroll();
+		redrawDotNav();
+    });
+    
+	/* Next/prev and primary nav btn click handlers */
+	$('a.life').click(function(){
+    	$('html, body').animate({
+    		scrollTop:0
+    	}, 1000, function() {
+	    	parallaxScroll(); // Callback is required for iOS
+		});
+    	return false;
 	});
-
-	$(window).bind('scroll',function(e){
-		footer();
-	});
+    $('a.srilanka').click(function(){
+    	$('html, body').animate({
+    		scrollTop:$('#srilanka').offset().top
+    	}, 1000, function() {
+	    	parallaxScroll(); // Callback is required for iOS
+		});
+    	return false;
+    });
+    $('a.japan').click(function(){
+    	$('html, body').animate({
+    		scrollTop:$('#japan').offset().top
+    	}, 1000, function() {
+	    	parallaxScroll(); // Callback is required for iOS
+		});
+    	return false;
+    });
+	$('a.about').click(function(){
+    	$('html, body').animate({
+    		scrollTop:$('#about').offset().top
+    	}, 1000, function() {
+	    	parallaxScroll(); // Callback is required for iOS
+		});
+    	return false;
+    });
+    
+    /* Show/hide dot lav labels on hover */
+    $('nav#primary a').hover(
+    	function () {
+			$(this).prev('h1').show();
+		},
+		function () {
+			$(this).prev('h1').hide();
+		}
+    );
+    
 });
 
-
+/* Scroll the background layers */
 function parallaxScroll(){
 	var scrolled = $(window).scrollTop();
 	$('#parallax-bg1').css('top',(0-(scrolled*.25))+'px');
@@ -18,8 +62,22 @@ function parallaxScroll(){
 	$('#parallax-bg3').css('top',(0-(scrolled*.75))+'px');
 }
 
-function footer() {
-	var footer_top = $(window).bottom;
-	//alert(footer_top);
-	$(".footer_wrap").css('top', footer_top +'px');
+/* Set navigation dots to an active state as the user scrolls */
+function redrawDotNav(){
+	var section1Top =  0;
+	// The top of each section is offset by half the distance to the previous section.
+	var section2Top =  $('#srilanka').offset().top - (($('#japan').offset().top - $('#srilanka').offset().top) / 2);
+	var section3Top =  $('#japan').offset().top - (($('#about').offset().top - $('#japan').offset().top) / 2);
+	var section4Top =  $('#about').offset().top - (($(document).height() - $('#about').offset().top) / 2);;
+	$('nav#primary a').removeClass('active');
+	if($(document).scrollTop() >= section1Top && $(document).scrollTop() < section2Top){
+		$('nav#primary a.life').addClass('active');
+	} else if ($(document).scrollTop() >= section2Top && $(document).scrollTop() < section3Top){
+		$('nav#primary a.srilanka').addClass('active');
+	} else if ($(document).scrollTop() >= section3Top && $(document).scrollTop() < section4Top){
+		$('nav#primary a.japan').addClass('active');
+	} else if ($(document).scrollTop() >= section4Top){
+		$('nav#primary a.about').addClass('active');
+	}
+	
 }

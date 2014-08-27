4731e73098155fdfdecebac6c2ddbcd450a0c2e1
kusmour <kxxxxx>
2014-08-27 16:55:59 +0800

    add loading

--- js/article_code.js
+++ js/article_code.js
@@ -2,4 +2,8 @@
 $(document).ready( function() {
 	$("pre").addClass("prettyprint linenums").attr('style', 'overflow:auto');;
 	prettyPrint();
-})
+});
+
+$(window).load( function() {
+	$("#loading").fadeOut();
+});
